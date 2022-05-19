3d65b2382d3ae020d8c0ad085f242b397bb04e1b
SabianF <58588133+SabianF@users.noreply.github.com>
2022-05-19 10:19:19 -0700

    docs: added helpful description & examples

--- lib/simple_future_builder.dart
+++ lib/simple_future_builder.dart
@@ -4,8 +4,40 @@ import 'package:flutter/material.dart';
 
 /// A simplified use-case of [FutureBuilder] that automatically
 /// handles errors, and requires only a [future] and a [childDone].
+/// 
 /// All loading states return a [_defaultLoadingWidget], and all
 /// normal error states return a [_defaultErrorWidget].
+/// 
+/// ---
+/// 
+/// [T] is assigned to an [AsyncSnapshot], and when the [Future] is done, the
+/// snapshot is processed for error states. After this, its data is cast as [T]
+/// where the [childDone] function handles it.
+/// 
+/// e.g. if [T] is a String, then [childDone] can look like this
+/// ```dart
+/// childDone = (context, stringData) => Text(stringData);
+/// ```
+/// 
+/// ---
+/// 
+/// Example usage:
+/// 
+/// ```dart
+/// SimpleFutureBuilder<String>(
+///   future: getAsyncStringData,
+///   childDone: (context, data) => Text(data),
+/// );
+/// ```
+/// 
+/// The above code returns a [Widget] which does the following
+/// - Awaits a [String] from a [Future]
+/// - If there is no [Future] being awaited, shows a [_defaultNoneWidget]
+/// - If the [AsyncSnapshot] is waiting or active, shows a [_defaultLoadingWidget]
+/// - If the [AsyncSnapshot] has an unknown status, or is done but has an error
+///   or returns no data, shows a [_defaultErrorWidget]
+/// - If the [AsyncSnapshot] is done, since no [dataValidator] was provided, simply
+///   shows the returned [String] as a [Text] widget
 class SimpleFutureBuilder<T> extends StatelessWidget {
 
   static const Widget _defaultNoneWidget =
